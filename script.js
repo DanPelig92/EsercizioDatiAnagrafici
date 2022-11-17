523df418d1df04a39bc53b4aa3d4b9cfc9a63cd7
Dan <you@example.com>
2022-11-17 12:55:57 +0100

    Funzionante ma brutto come la fame

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-import {User, User} from './User.js'
+import {User} from './User.js'
 
 let pulsanteSubmit = document.getElementById("submit")
 pulsanteSubmit.addEventListener("click",submit, false);
+let pulsanteVisualizzaDatiUtente = document.getElementById("visualizzaDati")
+pulsanteVisualizzaDatiUtente.addEventListener("click",stampaDati, false)
 
+
+let array=[]
 function submit(){
 
-    let User = new User(
+    let user = new User(
         document.getElementById("nomeUtente").value, 
         document.getElementById("cognomeUtente").value, 
         document.getElementById("codiceFiscaleUtente").value, 
@@ -14,12 +18,17 @@ function submit(){
         document.getElementById("emailUtente").value,
         document.getElementById("telefonoUtente").value,
         document.getElementById("occupazioneUtente").value)
-    // document.getElementById("nome").textContent = "Nome: " + document.getElementById("nomeUtente").value
-    // document.getElementById("cognome").textContent = "Cognome: " + document.getElementById("cognomeUtente").value
-    // document.getElementById("codiceFiscale").textContent = "Codice fiscale: " + document.getElementById("codiceFiscaleUtente").value //MDDDNL92M26D969P
-    // document.getElementById("dataNascita").textContent = "Data di nascita: " + document.getElementById("dataNascitaUtente").value
-    // document.getElementById("indirizzo").textContent = "Indirizzo: " + document.getElementById("indirizzoUtente").value
-    // document.getElementById("email").textContent = "Email: " + document.getElementById("emailUtente").value
-    // document.getElementById("telefono").textContent = "Telefono: " + document.getElementById("telefonoUtente").value
-    // document.getElementById("occupazione").textContent = "Occupazione: " + document.getElementById("occupazioneUtente").value
+        array[0] = user;
+        // array.push(user);
+}
+
+function stampaDati(){
+    document.getElementById("nome").textContent = "Nome: " + array[0].getNome()
+    document.getElementById("cognome").textContent = "Cognome: " + array[0].getCognome()
+    document.getElementById("codiceFiscale").textContent = "Codice fiscale: " + array[0].getCodiceFiscale() //MDDDNL92M26D969P
+    document.getElementById("dataNascita").textContent = "Data di nascita: " + array[0].getDataNascita()
+    document.getElementById("indirizzo").textContent = "Indirizzo: " + array[0].getIndirizzo()
+    document.getElementById("email").textContent = "Email: " + array[0].getEmail()
+    document.getElementById("telefono").textContent = "Telefono: " + array[0].getTelefono()
+    document.getElementById("occupazione").textContent = "Occupazione: " + array[0].getOccupazione()
 }
